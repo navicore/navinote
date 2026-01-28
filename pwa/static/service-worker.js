09949fc8edc29a271292b164fc4135b17528f5b1
Ed Sweeney <ed@onextent.com>
2026-01-27 21:20:55 -0800

    haha

--- pwa/static/service-worker.js
+++ pwa/static/service-worker.js
@@ -1,10 +1,20 @@
-const CACHE_NAME = 'navinote-v1';
+const CACHE_NAME = 'navinote-v2';
 const PRECACHE = ['/', '/index.html'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {

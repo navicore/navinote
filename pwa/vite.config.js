2448cf52cf37684d66aa0f395d6ae91c86796ba3
Ed Sweeney <ed@onextent.com>
2026-01-31 11:35:07 -0800

    icons

--- pwa/vite.config.js
+++ pwa/vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  publicDir: 'static',
   server: {
     proxy: {
       '/api': 'http://localhost:8080'
